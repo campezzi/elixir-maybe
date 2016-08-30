c9f1e30a4d66a15222b4e6d920910251881b5dc3
Thiago Campezzi <Thiago.ViccinoCampezzi@myob.com>
2016-08-30 10:01:09 +1000

    improve Maybe implementation

--- lib/maybe.ex
+++ lib/maybe.ex
@@ -8,30 +8,33 @@ defmodule Maybe do
   def just(value) do
     %Maybe{value: {:just, value}}
   end
+
+  def wrap(nil), do: Maybe.nothing
+  def wrap(value), do: Maybe.just(value)
 end
 
 defimpl Functor, for: Maybe do
-  def map(%{value: :nothing}, _function), do: Maybe.nothing
+  def map(%{value: :nothing} = nothing, _function), do: nothing
 
   def map(%{value: {:just, value}}, function) do
     value
     |> function.()
-    |> Maybe.just
+    |> Maybe.wrap
   end
 end
 
 defimpl Applicative, for: Maybe do
-  def ap(%Maybe{value: :nothing}, _), do: Maybe.nothing
-  def ap(%Maybe{value: {:just, value}}, %Maybe{value: :nothing}), do: Maybe.just(value)
+  def ap(%Maybe{value: :nothing} = nothing, _), do: nothing
+  def ap(%Maybe{value: {:just, _}} = value, %Maybe{value: :nothing}), do: value
   def ap(%Maybe{value: {:just, value}}, %Maybe{value: {:just, function}}) do
     value
     |> function.()
-    |> Maybe.just
+    |> Maybe.wrap
   end
 end
 
 defimpl Monad, for: Maybe do
-  def flat_map(%Maybe{value: :nothing}, _), do: Maybe.nothing
+  def flat_map(%Maybe{value: :nothing} = nothing, _), do: nothing
   def flat_map(%Maybe{value: {:just, value}}, function) do
     value
     |> function.()
